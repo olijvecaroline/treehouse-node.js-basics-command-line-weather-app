e75abb5602f6f952652b26ab578753ce59eb299e
Caroline Olijve <olijvecaroline@gmail.com>
2017-06-27 10:55:29 +0200

    error-handling added

--- weather.js
+++ weather.js
@@ -6,26 +6,38 @@ function printWeather(weather){
 	const message = `The temperature in ${weather.current_observation.observation_location.city } is ${weather.current_observation.temp_c} degrees`;
 	console.log(message);
 }
+//print errors
+function printError(){
+	console.log(http.STATUS_CODES[response.statusCode]);
+}
 
 function get(state, city){
-  var queryString = `https://api.wunderground.com/api/${api.key}/conditions/q/${state}/${city}.json`
-  const request = https.get(queryString, response => {
-                                   console.log(`responsecode : ${response.statusCode}`);
-     //reading the data
-      let body = "";
-      response.on('data', data => {
-        body += data;
-      });//end of response-on
+	try{
+	  var queryString = `https://api.wunderground.com/api/${api.key}/conditions/q/${state}/${city}.json`
+	  const request = https.get(queryString, response => {
+	      console.log(`responsecode : ${response.statusCode}`);
+				//checking statusCode
+				if(response.statusCode === 200){
+			     //reading the data
+			      let body = "";
+			      response.on('data', data => {
+			        body += data;
+			      });//end of response-on
 
-      //parsing the data
-      response.on('end',()=>{
-        console.log(queryString);
-        let weather = JSON.parse(body);
-        printWeather(weather);
-      });//end of on-end
-
-    });//end of get-request
-}
+			      //parsing the data
+			      response.on('end',()=>{
+			        console.log(queryString);
+			        let weather = JSON.parse(body);
+			        printWeather(weather);
+			      });//end of on-end
+				}else {
+					console.log(`error in retrieving the information`)
+				}
+	    });//end of get-request
+		}catch{
+			printError();
+		}//end of try-catch block
+}//end of get-function
 
 
 module.exports.get = get;
